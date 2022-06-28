4e5f00e1328853a77acfc50094246aadf7ecd740
Saeed <saeedofficial921@gmail.com>
2022-06-28 15:23:30 +0600

    added colors files

---
+++ lib/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const txtWhite= TextStyle(
+  color: Colors.white
+);
+const txtRed=TextStyle(
+  color: Colors.red
+);
+const txtBlue=TextStyle(
+    color: Colors.blue
+);
+const txtBluegray=TextStyle(
+    color: Colors.blueGrey
+);
+const txtBlack=TextStyle(
+    color: Colors.black
+);
+const txtYellow=TextStyle(
+    color: Colors.yellow
+);
+const txtGreen=TextStyle(
+    color: Colors.green
+);
